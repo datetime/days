28304afc785829ddcb434e6d7c43923a3b009189
jonschlinkert <dev@sellside.com>
2014-06-15 15:11:17 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * days <https://github.com/jonschlinkert/days>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+module.exports = ['Sunday', 'Monday', 'Tuesday', 'Wendesday', 'Thursday', 'Friday', 'Saturday'];
