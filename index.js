4bcce86558e7be9e0baadef5e2686229241d5d6b
Sebastien MEIFFREN <sebastien.meiffren@commande-online.fr>
2016-11-13 16:28:47 +0100

    Update to include multi-language support
    Adding French and updating for English

--- index.js
+++ index.js
@@ -5,6 +5,17 @@
  * Licensed under the MIT license.
  */
 
+// English
+module.exports.en = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+module.exports.en.abbr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+module.exports.en.short = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+
+// French translation
+module.exports.fr = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
+module.exports.fr.abbr = ['dim', 'lun', 'mar', 'mer', 'jeu', 'ven', 'sam'];
+module.exports.fr.short = ['di', 'lu', 'ma', 'me', 'je', 've', 'sa'];
+
+// In order not to break compatibility
 module.exports = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 module.exports.abbr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 module.exports.short = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
